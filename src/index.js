b467002cbccfbd1acd5736f3ae5286c6d1e1d390
94-mansi <144269382+94-mansi@users.noreply.github.com>
2023-11-10 13:52:11 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,22 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-reportWebVitals();import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
 
